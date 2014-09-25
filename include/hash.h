723061092eb2ff005de72a05acf7236344e44e9b
Vsevolod Stakhov <vsevolod@highsecure.ru>
2014-09-25 13:38:09 +0100

    Add jenkins hash as it seems to be faster for integers.

--- include/hash.h
+++ include/hash.h
@@ -426,6 +426,107 @@ static _hash_generic_hash_t _hash_murmur = {
   .hash_final = _hash_mur_final,
   .d = NULL
 };
+
+/*
+ * Generic Jenkins hash function
+ * http://eternallyconfuzzled.com/tuts/algorithms/jsw_tut_hashing.aspx
+ */
+struct _hash_jen_state {
+  uint32_t h;
+  uint32_t init;
+};
+
+#define JEN_MIX(a,b,c)                                                        \
+{                                                                              \
+  a -= b; a -= c; a ^= ( c >> 13 );                                            \
+  b -= c; b -= a; b ^= ( a << 8 );                                             \
+  c -= a; c -= b; c ^= ( b >> 13 );                                            \
+  a -= b; a -= c; a ^= ( c >> 12 );                                            \
+  b -= c; b -= a; b ^= ( a << 16 );                                            \
+  c -= a; c -= b; c ^= ( b >> 5 );                                             \
+  a -= b; a -= c; a ^= ( c >> 3 );                                             \
+  b -= c; b -= a; b ^= ( a << 10 );                                            \
+  c -= a; c -= b; c ^= ( b >> 15 );                                            \
+}
+
+static void* _hash_jen_init(void *d, void *space, unsigned spacelen)
+{
+  struct _hash_jen_state *s = (struct _hash_jen_state *)space;
+  s->h = 0xfeedbeef;
+  s->init = 0x9e3779b9;
+
+  return space;
+}
+
+static void _hash_jen_update (void *s, const unsigned char *k, size_t inlen,
+    void *d)
+{
+  struct _hash_jen_state *st = (struct _hash_jen_state *) s;
+  unsigned a, b;
+  unsigned c;
+  unsigned len = inlen;
+
+  c = st->init;
+  a = b = 0x9e3779b9;
+
+  while (len >= 12) {
+    a += (k[0] + ((unsigned) k[1] << 8) + ((unsigned) k[2] << 16)
+        + ((unsigned) k[3] << 24));
+    b += (k[4] + ((unsigned) k[5] << 8) + ((unsigned) k[6] << 16)
+        + ((unsigned) k[7] << 24));
+    c += (k[8] + ((unsigned) k[9] << 8) + ((unsigned) k[10] << 16)
+        + ((unsigned) k[11] << 24));
+
+    JEN_MIX(a, b, c);
+
+    k += 12;
+    len -= 12;
+  }
+
+  c += inlen;
+
+  switch (len) {
+  case 11:
+    c += ((unsigned) k[10] << 24);
+  case 10:
+    c += ((unsigned) k[9] << 16);
+  case 9:
+    c += ((unsigned) k[8] << 8);
+    /* First byte of c reserved for length */
+  case 8:
+    b += ((unsigned) k[7] << 24);
+  case 7:
+    b += ((unsigned) k[6] << 16);
+  case 6:
+    b += ((unsigned) k[5] << 8);
+  case 5:
+    b += k[4];
+  case 4:
+    a += ((unsigned) k[3] << 24);
+  case 3:
+    a += ((unsigned) k[2] << 16);
+  case 2:
+    a += ((unsigned) k[1] << 8);
+  case 1:
+    a += k[0];
+  }
+
+  JEN_MIX(a, b, c);
+  st->h = c;
+}
+
+HASH_TYPE _hash_jen_final(void *s, void *d)
+{
+  struct _hash_jen_state *st = (struct _hash_jen_state *)s;
+  return st->h;
+}
+
+static _hash_generic_hash_t _hash_jenkins = {
+  .hash_init = _hash_jen_init,
+  .hash_update = _hash_jen_update,
+  .hash_final = _hash_jen_final,
+  .d = NULL
+};
 /*
  * Generators part
  */
